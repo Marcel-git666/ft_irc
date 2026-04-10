3e71f15b4676ff57b184427d24dc4b599c02d751
Irina Arefeva <iarefeva@c3r5s4.42prague.com>
2026-04-10 16:48:05 +0200

    Unit test corrected in accordance with error messages and core dump from double free was fixed

--- unit_tests/tester_main.cpp
+++ unit_tests/tester_main.cpp
@@ -18,20 +18,21 @@ int test_ping(Server& server, FakeClient& client) {
 
 int main() {
 	Server server(6667, "123");
-	FakeClient alice(1, "127.0.0.1");
-	FakeClient bob(2, "127.0.0.2");
-	FakeClient judy(3, "127.0.0.3");
+	FakeClient* alice = new FakeClient(101, "127.0.0.1");
+	FakeClient* bob = new FakeClient(102, "127.0.0.2");
+	FakeClient* judy = new FakeClient(103, "127.0.0.3");
 
-	server.addClient(alice);
-	server.addClient(bob);
-	server.addClient(judy);
+	server.addClient(*alice);
+	server.addClient(*bob);
+	server.addClient(*judy);
 	
 	int i = 0;
-	i += test_registration(server, alice, bob, judy);
-	i += test_privat_msg(server, alice, bob, judy);
-	i += test_join(server, alice, bob, judy);
-	i += test_chan_logic(server, alice, bob, judy);
-	i += test_ping(server, alice);
+	i += test_registration(server, *alice, *bob, *judy);
+	i += test_privat_msg(server, *alice, *bob, *judy);
+	i += test_join(server, *alice, *bob, *judy);
+	i += test_chan_logic(server, *alice, *bob, *judy);
+	i += test_ping(server, *alice);
 	if (i == 0)
 		std::cout << PINK << "All tests PASSED!" << ENDCOLOR << std::endl;
+	return (i);
 }
